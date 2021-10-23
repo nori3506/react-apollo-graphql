676806ab607e9111930a01cd9c8fe191f36409b7
Nori <loveandfight2186@yahoo.co.jp>
2021-10-24 07:10:13 +0900

    create add contact form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Addcontact from "./components/form/AddContact";
 import Title from "./components/layouts/Title";
 
 const App = () => {
   return (
     <div className="App">
       <Title />
+      <Addcontact />
     </div>
   );
 };
--- client/src/components/form/AddContact.js
+++ client/src/components/form/AddContact.js
@@ -1,7 +1,49 @@
 import React, { useEffect, useState } from "react";
 import { Form, Input, Button } from "antd";
 const Addcontact = () => {
-  return <div></div>;
+  const [form] = Form.useForm();
+  const [, forceUpdate] = useState();
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
+  return (
+    <Form
+      form={form}
+      name="add-contact-form"
+      layout="inline"
+      size="large"
+      style={{ marginBottom: "40px" }}
+    >
+      <Form.Item
+        name="firstName"
+        rules={[{ required: true, message: "please input your first name" }]}
+      >
+        <Input placeholder="John" />
+      </Form.Item>
+
+      <Form.Item
+        name="lastName"
+        rules={[{ required: true, message: "please input your last name" }]}
+      >
+        <Input placeholder="Ropes" />
+      </Form.Item>
+      <Form.Item shouldUpdate={true}>
+        {() => (
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={
+              !form.isFieldTouched(true) ||
+              form.getFieldError().filter(({ errors }) => errors.length)
+            }
+          >
+            Add Contact
+          </Button>
+        )}
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Addcontact;

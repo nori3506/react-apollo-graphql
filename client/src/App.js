c16095079b7a6bad829d1e86b0ac1ae105021cab
Nori <loveandfight2186@yahoo.co.jp>
2021-10-24 07:22:23 +0900

    set Apollo Client in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
+import React from "react";
 import "./App.css";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Addcontact from "./components/form/AddContact";
 import Title from "./components/layouts/Title";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
-    <div className="App">
-      <Title />
-      <Addcontact />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Title />
+        <Addcontact />
+      </div>
+    </ApolloProvider>
   );
 };
 

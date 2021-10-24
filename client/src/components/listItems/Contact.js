21c99dcb3bcec6ed088c4c11de69447fa094aee5
Nori <loveandfight2186@yahoo.co.jp>
2021-10-24 14:32:17 +0900

    implement update contact function

--- client/src/components/listItems/Contact.js
+++ client/src/components/listItems/Contact.js
@@ -1,18 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "antd";
-
+import RemoveContact from "../buttons/RemoveContact";
+import { EditOutlined } from "@ant-design/icons";
+import UpdateContact from "../form/UpdateContact";
 const getStyles = () => ({
   card: { width: "500px" },
 });
 
 const Contact = (props) => {
-  const { id, firstName, lastName } = props;
   const styles = getStyles();
+  const [id] = useState(props.id);
+  const [firstName, setFirstName] = useState(props.firstName);
+  const [lastName, setLastName] = useState(props.lastName);
+  const [editMode, setEditMode] = useState(false);
+
+  const updateStateVariable = (variable, value) => {
+    switch (variable) {
+      case "firstName":
+        setFirstName(value);
+        break;
+      case "lastName":
+        setLastName(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleButtonClick = () => {
+    setEditMode(!editMode);
+  };
+
   return (
-    <Card style={styles.card}>
-      {firstName}
-      {lastName}
-    </Card>
+    <>
+      {editMode ? (
+        <UpdateContact
+          id={props.id}
+          firstName={props.firstName}
+          lastName={props.lastName}
+          onButtonClick={handleButtonClick}
+          updateStateVariable={updateStateVariable}
+        />
+      ) : (
+        <Card
+          style={styles.card}
+          actions={[
+            <EditOutlined
+              key="edit"
+              onClick={handleButtonClick}
+              id={id}
+              firstName={firstName}
+              lastName={lastName}
+            />,
+            <RemoveContact id={id} firstName={firstName} lastName={lastName} />,
+          ]}
+        >
+          {firstName}
+          {lastName}
+        </Card>
+      )}
+    </>
   );
 };
 
--- client/src/queries.js
+++ client/src/queries.js
@@ -1,4 +1,5 @@
 import { gql } from "@apollo/client";
+import { last } from "lodash";
 
 export const GET_CONTACTS = gql`
   {
@@ -13,6 +14,30 @@ export const GET_CONTACTS = gql`
 export const ADD_CONTACT = gql`
   mutation AddContact($id: String!, $firstName: String!, $lastName: String!) {
     addContact(id: $id, firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const UPDATE_CONTACT = gql`
+  mutation UpdateContact(
+    $id: String!
+    $firstName: String!
+    $lastName: String!
+  ) {
+    updateContact(id: $id, firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export const REMOVE_CONTACT = gql`
+  mutation RemoveContact($id: String!) {
+    removeContact(id: $id) {
       id
       firstName
       lastName
